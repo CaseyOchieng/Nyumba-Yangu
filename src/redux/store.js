13d4253f778eec830fc182b84fbaf8b6164b7abe
Casey Ochieng <caseyochieng46@gmail.com>
2024-12-01 02:08:48 +0300

    Added persistor redux --No prombles yet--

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userSlice.js";
+
+//
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+// persist configuration
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+//making persistent store
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
