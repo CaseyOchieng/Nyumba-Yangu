a2e05c18abbc542c93af61f6c711b8cee40306a2
Casey Ochieng <caseyochieng46@gmail.com>
2024-12-12 16:19:51 +0300

    Profile page UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -12,7 +14,6 @@ export default function Header() {
             <span className="text-slate-800"> {"Plug"}</span>
           </h1>
         </Link>
-        {/* Other header content */}
         <form
           className="bg-slate-100 rounded-lg flex items-center"
           action="/search"
@@ -23,27 +24,29 @@ export default function Header() {
             type="text"
             name="q"
             placeholder="Search"
-          ></input>
+          />
           <FaSearch className="text-slate-600 mr-3 " />
         </form>
-        {/* Other header content */}
         <ul className="flex gap-4">
           <li className="hidden sm:inline text-slate-700 hover:text-slate-900 transition duration-200 ease-in-out">
-            <Link to="/home">Home</Link>
+            <Link to="/">Listings</Link>
           </li>
           <li className="hidden sm:inline text-slate-700 hover:text-slate-900 transition duration-200 ease-in-out">
             <Link to="/about">About</Link>
           </li>
-          {/* we will show avatar if the user is logged in otherwise we will show log in */}
           <Link to="/Profile">
             {currentUser ? (
               <img
                 className="rounded-full w-7 h-7 object-cover"
-                src={currentUser.avatar}
+                src={
+                  currentUser.avatar ? currentUser.avatar : "default-avatar.png"
+                }
                 alt={currentUser.username}
+                width={30}
+                height={30}
               />
             ) : (
-              <li className=" text-slate-700 hover:text-slate-900 transition duration-200 ease-in-out">
+              <li className="text-slate-700 hover:text-slate-900 transition duration-200 ease-in-out">
                 Signin
               </li>
             )}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,57 @@
+import { useSelector } from "react-redux";
+import Header from "../components/Header";
 export default function Profile() {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <>
-      <h1>Profile</h1>
+      <Header />
+      <div className="max-w-lg mx-auto p-4">
+        {/* Profile page content */}
+        <h1 className="text-3xl font-semibold text-center my-7 text-slate-800">
+          Profile
+        </h1>
+        {/* Form to update user information */}
+        <form className="flex flex-col">
+          {/* Form fields */}
+          <form>
+            {/* Avatar upload */}
+            <img
+              className="w-24 h-24 rounded-full object-cover cursor-pointer mx-auto mt-2 mb-6"
+              src={currentUser.avatar}
+              alt={currentUser.username}
+            />
+            {/* Form fields */}
+            <input
+              className="border-2 border-slate-300 rounded-lg p-2 w-full mb-6"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+            {/* Form fields */}
+            <input
+              className="border-2 border-slate-300 rounded-lg p-2 w-full"
+              id="email"
+              type="text"
+              placeholder="Email"
+            />
+            {/* Form fields */}
+            <input
+              className="border-2 border-slate-300 rounded-lg p-2 w-full mt-6 "
+              id="username"
+              type="text"
+              placeholder="Password"
+            />
+            {/* Submit button */}
+            <button
+              className="bg-slate-700 text-white rounded-lg p-2 w-full mt-6 hover:bg-slate-900 transition duration-200 ease-in-out"
+              type="submit"
+            >
+              Update Profile
+            </button>
+          </form>
+        </form>
+      </div>
     </>
   );
 }
